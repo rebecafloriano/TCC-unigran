fc75e591e73247e63eb9d3a0aa5acafe2f6e1426
Rebeca Erdman <rebecaoliver09@gmail.com>
2021-12-04 19:36:06 +0000

    fazendo login e tendo problemas com base api

--- mobile/src/screens/SignIn/index.js
+++ mobile/src/screens/SignIn/index.js
@@ -1,11 +1,83 @@
-import React from 'react';
+import React, {useState } from 'react';
+import { useNavigation } from '@react-navigation/native'
 import { Text } from 'react-native';
-import { Container } from './styles';
+import {
+     Container,
+     InputArea,
+     CustomButton,
+     CustomButtonText,
+     SignMessageButton,
+     SignMessageButtonText,
+     SignMessageButtonTextBold
+} from './styles';
+
+import Api from '../../Api';
+
+import SignInput from '../../components/SignInput';
+
+import PreloadMedico from '../../assets/preload.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+
+    const navigation = useNavigation();
+
+    const [emailField, setEmailField] = useState('');
+    const [passwordField, setPasswordField] = useState('');
+
+    const handleSignClick = async () => {
+        if(emailField != '' && passwordField != '') {
+
+            let json = await Api.signIn(emailField, passwordField);
+            console.log(json);
+            if(json.token) {
+                alert("DEU CERTO");
+            } else {
+                alert("E-mail e/ou senha errados!");
+            }
+
+        } else {
+            alert("Preencha os campos!");
+        }
+    }
+
+    const handleMessageButtonClick = () => {
+        navigation.reset({
+            routes: [{name: 'SignUp'}]
+        });
+    }
+
     return (
         <Container>
-            <Text>SignIn</Text>
+            <PreloadMedico width="100%" height="150" />
+
+            <InputArea>
+                <SignInput
+                 IconSvg={EmailIcon}
+                 placeholder="Digite seu e-mail"
+                 value={emailField}
+                 onChangeText={t=>setEmailField(t)}
+                />
+
+                <SignInput
+                 IconSvg={LockIcon}
+                 placeholder="Digite sua senha"
+                 value={passwordField}
+                 onChangeText={t=>setPasswordField(t)}
+                 password={true}
+                />
+
+                <CustomButton onPress={handleSignClick}>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignMessageButton onPress={handleMessageButtonClick}>
+                <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+            </SignMessageButton>
+
         </Container>
-    )
+    );
 }
--- mobile/src/screens/SignUp/index.js
+++ mobile/src/screens/SignUp/index.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, {useState } from 'react';
+import { useNavigation } from '@react-navigation/native'
 import { Text } from 'react-native';
-import { Container } from './styles';
+import {
+     Container,
+     InputArea,
+     CustomButton,
+     CustomButtonText,
+     SignMessageButton,
+     SignMessageButtonText,
+     SignMessageButtonTextBold
+} from './styles';
+
+import SignInput from '../../components/SignInput';
+
+import PreloadMedico from '../../assets/preload.svg';
+import PersonIcon from '../../assets/person.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+
+    const navigation = useNavigation();
+
+    const [nameField, setNameField] = useState('');
+    const [emailField, setEmailField] = useState('');
+    const [passwordField, setPasswordField] = useState('');
+
+    const handleSignClick = () => {
+
+    }
+
+    const handleMessageButtonClick = () => {
+        navigation.reset({
+            routes: [{name: 'SignIn'}]
+        });
+    }
+
     return (
         <Container>
-            <Text>signUp</Text>
+            <PreloadMedico width="100%" height="150" />
+
+            <InputArea>
+
+                <SignInput
+                 IconSvg={PersonIcon}
+                 placeholder="Digite seu nome"
+                 value={nameField}
+                 onChangeText={t=>setNameField(t)}
+                />
+
+                <SignInput
+                 IconSvg={EmailIcon}
+                 placeholder="Digite seu e-mail"
+                 value={emailField}
+                 onChangeText={t=>setEmailField(t)}
+                />
+
+                <SignInput
+                 IconSvg={LockIcon}
+                 placeholder="Digite sua senha"
+                 value={passwordField}
+                 onChangeText={t=>setPasswordField(t)}
+                 password={true}
+                />
+
+                <CustomButton onPress={handleSignClick}>
+                    <CustomButtonText>CADASTRAR</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignMessageButton onPress={handleMessageButtonClick}>
+                <SignMessageButtonText>Já possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Faça Login</SignMessageButtonTextBold>
+            </SignMessageButton>
+
         </Container>
-    )
+    );
 }
